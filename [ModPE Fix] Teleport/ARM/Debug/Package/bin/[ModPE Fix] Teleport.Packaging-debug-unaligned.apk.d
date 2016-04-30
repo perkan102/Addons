a3db092882bbe6273dec4a68ef8daf63b755f8e5
perkan102 <dlyoung12345@gmail.com>
2016-04-30 22:17:08 +0900

    Remove!
    Remove!

--- [ModPE Fix] Teleport/ARM/Debug/Package/bin/[ModPE Fix] Teleport.Packaging-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-C:\Users\dlyou\Documents\Visual Studio 2015\Projects\[ModPE Fix] Teleport\ARM\Debug\Package\bin\[ModPE Fix] Teleport.Packaging-debug-unaligned.apk : \
-C:\Users\dlyou\Documents\Visual Studio 2015\Projects\[ModPE Fix] Teleport\ARM\Debug\Package\bin\[ModPE Fix] Teleport.Packaging.ap_ \
-C:\Users\dlyou\Documents\Visual Studio 2015\Projects\[ModPE Fix] Teleport\ARM\Debug\Package\libs\armeabi-v7a\gdbserver \
-C:\Users\dlyou\Documents\Visual Studio 2015\Projects\[ModPE Fix] Teleport\ARM\Debug\Package\libs\armeabi-v7a\libcom.perkan.modpe_tpfix.so \
-C:\Users\dlyou\Documents\Visual Studio 2015\Projects\[ModPE Fix] Teleport\ARM\Debug\Package\libs\armeabi-v7a\libgnustl_shared.so \
--- [ModPE Fix] Teleport/ARM/Debug/Package/bin/[ModPE Fix] Teleport.Packaging.ap_.d
+++
@@ -1,3 +0,0 @@
-C:\Users\dlyou\Documents\Visual Studio 2015\Projects\[ModPE Fix] Teleport\ARM\Debug\Package\bin\[ModPE Fix] Teleport.Packaging.ap_ \
- : C:\Users\dlyou\Documents\Visual Studio 2015\Projects\[ModPE Fix] Teleport\ARM\Debug\Package\res\values\strings.xml \
-C:\Users\dlyou\Documents\Visual Studio 2015\Projects\[ModPE Fix] Teleport\ARM\Debug\Package\bin\AndroidManifest.xml \
